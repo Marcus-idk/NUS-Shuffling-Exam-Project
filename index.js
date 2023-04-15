e9afa93b783c2129660554a66ed260ea34ed079d
Marcus-idk <82570710+Marcus-idk@users.noreply.github.com>
2023-04-15 18:04:59 +0800

    Review

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-// parameters: what is "string"?
-// better optiosn for names: url/link
-// to make this more flexible: add an optional parameter for 'options'
-// wym by this? (down)
-// you can make moduleInfo more flexible by varying the year, eg:
-// await fetchData("https://api.nusmods.com/v2/{dateRange}/moduleInfo.json" -> then take dateRange from user input
 let date = "2022-2023";
+let testData = [["CS1101S", "CS2030S", "CS2040S"], ["CS2040S", "CS3230"]]
 async function fetchData(link, options = {}) {
     return fetch(link, options).then(x => x.json());
 }
@@ -12,31 +7,26 @@ submitForm();
 async function submitForm() {
     let moduleInfo = await fetchData(`https://api.nusmods.com/v2/${date}/moduleInfo.json`);
     moduleInfo = removeModulesWithoutExamsAndinSem3or4(moduleInfo);
+    addCurrStartEndAttributes(moduleInfo);
+    addNewStartAndEndAttributes(moduleInfo);
     const map = mapModuleCodeToHashmap(moduleInfo);
-    addStartEndAttributes(moduleInfo);
     sortExamDatesASC(moduleInfo);
-    let partitions = findAllPartitionsOfDifferentLengths(moduleInfo);
-    let maxLength = maxPartition(partitions);
-    console.log(moduleInfo);
-    console.log(partitions);
-    console.log(maxLength);
+    let grouped = groupByDay(moduleInfo);
+    let metricOld = metricForAllStudents(testData, map)
+    shuffle(grouped)
+    updateResultsMetric(metricOld, testData, grouped, map)
 }
 /*
 1) Remove modules without exams (using filter)
 1.25) split modules
 1.5) add attributes to every module: currStart, currEnd
 2) sort exams based on the json attributes, exam date/time asc order
-3) use merge intervals algorithm to find the partitions of modules of different lengths
-4) find largest partition and the corresponding length
-4.5) assign every module a group attribute in JSON
-5) adjust all partitions to be the selected length
-    - if there is spacing in front of the partition, move the partition in front/ extend forwards, stop when < 8am
-    - if there is spacing at the back, move back/extend backwards, stop when exams > 9pm
-6) shuffle the timeslots (all equal length) using fisher-yates shuffling algorithm (refer to notes)
-7) change the exam dates/times of the modules to new timeslot
-7.5) add newStart, newEnd to all modules
-8) make the metric calculator
-9) make simple tests, eg of format:
+3) group the exams up by their day
+4) add newStart, newEnd to all modules
+5) shuffle the timeslots (all mods on day 1 -> day 2) (refer to notes)
+6) change the exam dates/times of the modules to new timeslot
+7) make the metric calculator
+8) make simple tests, eg of format:
     function testFetchData() {
         const input = // input here
         const expected = // expected data
@@ -59,14 +49,39 @@ function mapModuleCodeToHashmap(examsJSON) {
     })
     return map;
 }
-function isClash(date1, date2) {
-    return date1 >= date2;
+function isSameDay(date1, date2) {
+    return date1.toDateString() === date2.toDateString();
 }
 function getCurrStartDate(moduleObject) {
     return new Date(moduleObject.semesterData[0].currStart);
 }
 function getCurrEndDate(moduleObject) {
     return new Date(moduleObject.semesterData[0].currEnd);
+}
+function getNewStartDate(moduleObject) {
+    // console.log(moduleObject)
+    // console.log(moduleObject.semesterData[0].newStart)
+    // console.log("bruh")
+    return new Date(moduleObject.semesterData[0].newStart);
+}
+function getNewEndDate(moduleObject) {
+    return new Date(moduleObject.semesterData[0].newEnd);
+}
+function changeCurrStartDateDay(moduleObject, day) {
+    moduleObject.semesterData[0].currStart.setDate(day)
+}
+function changeCurrEndDateDay(moduleObject, day) {
+    moduleObject.semesterData[0].currEnd.setDate(day)
+}
+function changeNewStartDateDay(moduleObject, day) {
+    const newDate = new Date(moduleObject.semesterData[0].newStart)
+    newDate.setDate(day)
+    moduleObject.semesterData[0].newStart = newDate.toISOString();
+}
+function changeNewEndDateDay(moduleObject, day) {
+    const newDate = new Date(moduleObject.semesterData[0].newEnd)
+    newDate.setDate(day)
+    moduleObject.semesterData[0].newEnd = newDate.toISOString()
 }
 //end of extra logic
 function removeModulesWithoutExamsAndinSem3or4(examsJSON) {
@@ -75,7 +90,7 @@ function removeModulesWithoutExamsAndinSem3or4(examsJSON) {
     return modsInSem1And2;
 }
 //main steps
-function addStartEndAttributes(examsJSON) {
+function addCurrStartEndAttributes(examsJSON) {
     examsJSON.forEach(x => {
         const exam = x.semesterData[0];
         exam.currStart = exam.examDate;
@@ -89,26 +104,70 @@ function sortExamDatesASC(examsJSON) {
         return firstDate - secondDate;
     })
 }
-function findAllPartitionsOfDifferentLengths(examsJSON) {
-    let arrOfPartitions = [[]];
+function groupByDay(examsJSON) {
+    let arrOfPartitons = [[]];
     for (let i = 0; i < examsJSON.length; i++) {
         if (i == 0) {
-            arrOfPartitions[0].push(examsJSON[i]);
-        } else if (isClash(getCurrEndDate(examsJSON[i - 1]), getCurrStartDate(examsJSON[i]))) {
-            arrOfPartitions.push([]);
-            arrOfPartitions[arrOfPartitions.length - 1].push(examsJSON[i]);
+            arrOfPartitons[0].push(examsJSON[i]);
+        } else if (isSameDay(getCurrStartDate(examsJSON[i - 1]), getCurrStartDate(examsJSON[i]))) {
+            arrOfPartitons[arrOfPartitons.length - 1].push(examsJSON[i]);
         } else {
-            arrOfPartitions[arrOfPartitions.length - 1].push(examsJSON[i]);
+            arrOfPartitons.push([]);
+            arrOfPartitons[arrOfPartitons.length - 1].push(examsJSON[i]);
         }
     }
-    return arrOfPartitions;
+    return arrOfPartitons;
 }
-function maxPartition(partitions) {
-    let maxLength = -Infinity;
-    partitions.forEach(x => {
-        if (x.length > maxLength) {
-            maxLength = x.length;
-        }
+function addNewStartAndEndAttributes(examsJSON) {
+    examsJSON.forEach(x => {
+        const exam = x.semesterData[0];
+        exam.newStart = exam.currStart;
+        exam.newEnd = exam.currEnd;
     })
-    return maxLength;
 }
+function shuffle(partitions) {
+    function swapPartitions(partition1, partition2) {
+        let partition1Day = getCurrEndDate(partition1[0]).getDay();
+        let partition2Day = getCurrEndDate(partition2[0]).getDay();
+        for (let i = 0; i < partition1.length; i++) {
+            changeNewStartDateDay(partition1[i], partition2Day)
+            changeNewEndDateDay(partition1[i], partition2Day)
+        }
+        for (let i = 0; i < partition2.length; i++) {
+            changeNewStartDateDay(partition2[i], partition1Day)
+            changeNewEndDateDay(partition2[i], partition1Day)
+        }
+    }
+    for (let i = partitions.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        swapPartitions(partitions[i], partitions[j]);
+    }
+    return partitions;
+}
+function metricForOneStudent(studentTimeTable, map) {
+    let res = 0;
+    console.log(studentTimeTable)
+    for (let i = 0; i < studentTimeTable.length - 1; i++) {
+        res += Math.abs(getNewStartDate(map.get(studentTimeTable[i + 1])).getDay() - getNewEndDate(map.get(studentTimeTable[i])).getDay())
+    }
+    return res / (studentTimeTable.length - 1)
+}
+function metricForAllStudents(studentsTimeTable, map) {
+    let res = 0;
+    for (let i = 0; i < studentsTimeTable.length; i++) {
+        res += metricForOneStudent(studentsTimeTable[i], map)
+    }
+    return res / studentsTimeTable.length
+}
+function updateResultsMetric(oldMetricValue, testData, partitions, map) {
+    const prevMetric = document.querySelector('#resultsMetric strong.red');
+    const newMetric = document.querySelector('#resultsMetric strong.green');
+    let newMetricValue = metricForAllStudents(testData, map)
+    while (newMetricValue <= oldMetricValue) {
+      shuffle(partitions);
+      newMetricValue = metricForAllStudents(testData, map);
+    }
+    prevMetric.innerHTML = oldMetricValue.toFixed(2);
+    newMetric.innerHTML = newMetricValue.toFixed(2);
+  }
+  
